a83fd44b3e98e502e9eed4f2769319c625ca72b1
Terren Culberson <culbersonterren@yahoo.com>
2020-01-26 02:44:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,7 @@
 class Person
   #your code here
-  attr_accessor  
+  attr_accessor :name, :birthday, :hair_color, :eye_color, 
 
   def initialize(:name, :birthday, :hair_color, :eye_color)
+    
 end
